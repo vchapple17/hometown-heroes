7529fe451085e2bc4a28089cc47c6afda57062b5
vchapple17 <vchapple17@gmail.com>
2017-08-01 18:24:06 -0500

    Temp FB auth file

---
+++ server/config/auth.js
@@ -0,0 +1,8 @@
+// Taken from FB Spike. Possibly need to adjust
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '263087834190352',
+		'clientSecret': 'f06fdc70a0a796673b3423bdf340eb1f',
+		'callbackURL': 'http://flip2.engr.oregonstate.edu:16661/facebook/callback'
+	}
+}
