9f97894002bdb3212a05103b18e0e5b03fbfd459
vchapple17 <vchapple17@gmail.com>
2017-08-02 15:15:21 -0500

    twitter auth info

--- server/config/auth.js
+++ server/config/auth.js
@@ -4,5 +4,10 @@ module.exports = {
 		'clientID': '263087834190352',
 		'clientSecret': 'f06fdc70a0a796673b3423bdf340eb1f',
 		'callbackURL': 'http://flip2.engr.oregonstate.edu:16661/facebook/callback'
+	},
+	'twitterAuth' : {
+		'consumerKey': 'uuN5yhYRz00wf7KTohhfT1RQg',
+		'consumerSecret': 'S320dXit2DXYlIERX2behoN9s5W0zNJFc5iObZGIGPHTlMDfp6',
+		'callbackURL': 'http://flip2.engr.oregonstate.edu:16661/auth/twitter/callback'
 	}
 }
