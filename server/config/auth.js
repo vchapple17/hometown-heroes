a484c63de50f677f7e82e222deb77be449e9a3fb
GNIEBANCK <niebancg@oregonstate.edu>
2017-08-08 13:44:33 -0400

    Update auth.js

--- server/config/auth.js
+++ server/config/auth.js
@@ -9,7 +9,7 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID': '263087834190352',
 		'clientSecret': 'f06fdc70a0a796673b3423bdf340eb1f',
-		'callbackURL': 'http://flip2.engr.oregonstate.edu:16661/login/facebook/callback'
+		'callbackURL': 'http://localhost:16661/login/facebook/callback'
 	},
 	'twitterAuth' : {
 		'consumerKey': 'uuN5yhYRz00wf7KTohhfT1RQg',
