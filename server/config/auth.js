a129205d418e9729a1efb22054efaf95047a24e1
vchapple17 <vchapple17@gmail.com>
2017-08-08 12:01:30 -0500

    localhost instead of flip2

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,4 +1,10 @@
 // Taken from FB Spike. Possibly need to adjust
+
+// Flip:
+//http://flip2.engr.oregonstate.edu:16661/login/facebook/callback
+
+// localhost:
+// http://127.0.0.1:16661/login/facebook/callback
 module.exports = {
 	'facebookAuth' : {
 		'clientID': '263087834190352',
@@ -8,6 +14,6 @@ module.exports = {
 	'twitterAuth' : {
 		'consumerKey': 'uuN5yhYRz00wf7KTohhfT1RQg',
 		'consumerSecret': 'S320dXit2DXYlIERX2behoN9s5W0zNJFc5iObZGIGPHTlMDfp6',
-		'callbackURL': 'http://flip2.engr.oregonstate.edu:16661/login/twitter/callback'
+		'callbackURL': 'http://127.0.0.1:16661/login/twitter/callback'
 	}
 }
