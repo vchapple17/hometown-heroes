1c2d8344e186e3e22adf102b8dae623824814fec
Val <vchapple17@users.noreply.github.com>
2017-08-14 20:56:52 -0500

    Update dbcon.js

--- server/db/dbcon.js
+++ server/db/dbcon.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 30,
-  host            : 'localhost',
-  user            : 'valchapple',
-  password        : 'hometown',
-  database        : 'hh_db',
+  host            : '',
+  user            : '',
+  password        : '',
+  database        : '',
 });
